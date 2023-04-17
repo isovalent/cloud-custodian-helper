81026475cb78973320c2c216cf214c49c046c7a9
viktor-kurchenko <69600804+viktor-kurchenko@users.noreply.github.com>
2023-04-17 12:05:28 +0300

    Slack notification improved.
    Signed-off-by: viktor-kurchenko <69600804+viktor-kurchenko@users.noreply.github.com>

--- cmd/slack.go
+++ cmd/slack.go
@@ -16,7 +16,7 @@ var slackCmd = &cobra.Command{
 	Run:     notify,
 }
 
-var slackResourceFile, slackToken, slackChannel, slackMembersFile, slackTitle *string
+var slackResourceFile, slackToken, slackChannel, slackTitle *string
 
 func init() {
 	slackResourceFile = slackCmd.Flags().StringP("resource-file", "r", "resources.json", "Resource JSON file")
@@ -25,15 +25,13 @@ func init() {
 	_ = slackCmd.MarkFlagRequired("auth-token")
 	slackChannel = slackCmd.Flags().StringP("channel", "c", "", "Slack default channel ID")
 	_ = slackCmd.MarkFlagRequired("channel")
-	slackMembersFile = slackCmd.Flags().StringP("members", "m", "", "Slack members YAML file")
-	_ = slackCmd.MarkFlagFilename("members")
 	slackTitle = slackCmd.Flags().StringP("title", "t", "", "Slack notification title")
 	rootCmd.AddCommand(slackCmd)
 }
 
 func notify(_ *cobra.Command, _ []string) {
 	ctx := context.Background()
-	if err := slack.Notify(ctx, *slackResourceFile, *slackToken, *slackChannel, *slackMembersFile, *slackTitle); err != nil {
+	if err := slack.Notify(ctx, *slackResourceFile, *slackToken, *slackChannel, *slackTitle); err != nil {
 		log.FromContext(ctx).Fatal(err)
 	}
 }
---
+++ pkg/slack/notify.go
@@ -0,0 +1,93 @@
+package slack
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"sort"
+
+	"c7n-helper/pkg/dto"
+	"c7n-helper/pkg/log"
+	"github.com/lensesio/tableprinter"
+)
+
+type msgLine struct {
+	Index   int    `header:"#"`
+	Region  string `header:"Region"`
+	Name    string `header:"Name"`
+	Created string `header:"Created date"`
+}
+
+func Notify(ctx context.Context, resourceFile, slackToken, slackDefaultChannel, title string) error {
+	logger := log.FromContext(ctx)
+	logger.Info("reading resource file...")
+	var report dto.PolicyReport
+	if err := report.ReadFromFile(resourceFile); err != nil {
+		return err
+	}
+	if len(report.Accounts) == 0 {
+		logger.Info("nothing to send")
+		return nil
+	}
+	slack := newSlackProvider(slackToken, title, slackDefaultChannel)
+	logger.Info("reading slack users...")
+	if err := slack.readUsers(ctx); err != nil {
+		return err
+	}
+	logger.Info("preparing slack messages...")
+	channelMessages := prepareSlackMessage(groupSlackMessage(report.Accounts, slack))
+	logger.Info("sending slack notification...")
+	return slack.notify(ctx, channelMessages)
+}
+
+// Groups Slack messages: SlackChannelID -> Account|Project|Subscription -> []Resources
+func groupSlackMessage(accounts []dto.Account, slack *slackProvider) map[string]map[string][]dto.Resource {
+	groups := make(map[string]map[string][]dto.Resource)
+	for _, account := range accounts {
+		for _, resource := range account.Resources {
+			channel := slack.getSlackIDByOwner(resource.Owner)
+			accountResources, ok := groups[channel]
+			if !ok {
+				accountResources = make(map[string][]dto.Resource)
+				groups[channel] = accountResources
+			}
+			if _, ok := accountResources[account.Name]; !ok {
+				accountResources[account.Name] = make([]dto.Resource, 0)
+			}
+			accountResources[account.Name] = append(accountResources[account.Name], resource)
+		}
+	}
+	return groups
+}
+
+func prepareSlackMessage(groups map[string]map[string][]dto.Resource) map[string][]string {
+	channelMessages := make(map[string][]string)
+	for channel, accountResources := range groups {
+		channelMessages[channel] = make([]string, 0)
+		for account, resources := range accountResources {
+			channelMessages[channel] = append(channelMessages[channel], fmt.Sprintf("*%s*\n", account))
+			sort.Slice(resources, func(i, j int) bool {
+				return resources[i].Created.Before(resources[j].Created)
+			})
+			buf := bytes.NewBufferString("")
+			tableprinter.Print(buf, normalizeDTO(resources))
+			for _, message := range splitMessage(buf.String()) {
+				channelMessages[channel] = append(channelMessages[channel], fmt.Sprintf("```\n%s```\n", message))
+			}
+		}
+	}
+	return channelMessages
+}
+
+func normalizeDTO(resources []dto.Resource) []msgLine {
+	result := make([]msgLine, 0, len(resources))
+	for i, r := range resources {
+		result = append(result, msgLine{
+			Index:   i + 1,
+			Region:  r.Location,
+			Name:    r.Name,
+			Created: r.Created.Format("2006-01-02"),
+		})
+	}
+	return result
+}
