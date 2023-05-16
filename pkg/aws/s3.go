793ab03201ea5337229856bf0c4da3ceb6bda491
viktor-kurchenko <69600804+viktor-kurchenko@users.noreply.github.com>
2023-05-16 10:58:18 +0300

    S3 mapping fixed.
    Signed-off-by: viktor-kurchenko <69600804+viktor-kurchenko@users.noreply.github.com>

--- pkg/aws/s3.go
+++ pkg/aws/s3.go
@@ -7,17 +7,27 @@ import (
 	"c7n-helper/pkg/dto"
 )
 
+type location struct {
+	LocationConstraint string `json:"LocationConstraint"`
+}
+
 func ParseS3(region string, content []byte) ([]dto.Resource, error) {
 	var buckets []struct {
 		Name      string     `json:"Name"`
 		CreatedAt time.Time  `json:"CreationDate"`
 		Tags      []keyValue `json:"Tags"`
+		Location  location   `json:"Location"`
 	}
 	if err := json.Unmarshal(content, &buckets); err != nil {
 		return nil, err
 	}
 	result := make([]dto.Resource, 0, len(buckets))
 	for _, bucket := range buckets {
+		if bucket.Location.LocationConstraint != region {
+			// custodian returns all buckets for each region
+			// skip buckets from another regions
+			continue
+		}
 		owner := ""
 		for _, tag := range bucket.Tags {
 			if tag.Key == "owner" {
